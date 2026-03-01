5d156f0e415c16daeae3a9c07bdf3625267eacab
Zoe González Rodríguez <zgonzalezr2001@alumno.ipn.mx>
2026-02-28 23:48:43 -0600

    Arregla errores en la integracion de Fourth y ThirdActivity

--- Practica1/app/src/main/java/com/example/practica1/FourthActivity.kt
+++ Practica1/app/src/main/java/com/example/practica1/FourthActivity.kt
@@ -5,22 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
-<<<<<<<< HEAD:Practica1/app/src/main/java/com/example/practica1/FourthActivity.kt
 class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_fourth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintPrincipal4)) { v, insets ->
-========
-class ThirdActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_third)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
->>>>>>>> 6539d1591f4514d46b0ce0488f274bedb03140ab:Practica1/app/src/main/java/com/example/practica1/ThirdActivity.kt
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
